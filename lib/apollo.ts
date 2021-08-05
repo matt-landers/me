e9cd54050de617c2bb473dfe9b1a52c0ee3f4305
Matt Landers <matt@platypi.io>
2021-08-05 10:38:36 -0500

    fixed link

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -2,7 +2,7 @@ import { withApollo } from "next-apollo";
 import ApolloClient, { InMemoryCache } from "apollo-boost";
 
 export const apolloClient = new ApolloClient({
-  uri: "https://mattlanders.wpengine.com/graphql",
+  uri: "https://mattlanders24.wpengine.com/graphql",
   cache: new InMemoryCache(),
 });
 
